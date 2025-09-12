3de50fb1bc0a8b9bf4662685d56a09a822053a0e
GitHub Action <action@github.com>
2025-09-12 08:39:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/e71828/exstcodesys/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/e71828/exstcodesys/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.e71828.exstcodesys.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
